cd3f88a963bd08d244df6e6685135307f4f82443
ML5W3S <marcelolauriano@gmail.com>
2023-01-09 15:22:41 -0300

    Abrasileirando

--- mensagens/produtor.py
+++ mensagens/produtor.py
@@ -5,11 +5,11 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 # Declaração da fila
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='opa')
 
 # Envia a mensagem
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+channel.basic_publish(exchange='', routing_key='hello', body='salve!')
+print(" [x] Enviar um salve!'")
 
 # Fecha a conexão
 connection.close()
